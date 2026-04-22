ef658d64f590fc1b6f60cdbb5aed595eb101d933
fanghey <2603Lysenko2603@gmail.com>
2026-04-22 19:51:46 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3:
-        print("Der Code ist gefälscht, es sollten 2 Schritte statt 3 sein.")
         return
 
     if parts[0] != "cp":
